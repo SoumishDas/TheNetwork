b90f68dd8212ca81fe7bb45699ee9a45a753c257
Lord Raleigh <you@example.com>
2023-02-24 08:24:32 +0530

    committing for the 1st time

--- src/network.cpp
+++ src/network.cpp
@@ -1,23 +1,52 @@
+//Header Files
 #include <iostream>
 #include <vector>
-
 using namespace std;
 
 
-int input[10] = {2,2,2,2,2};
-int weight[10] = {2,2,2,2,2};
-int bias[10] = {2,2,2,2,2};
 
+
+
+
+//Classes
 class Node{
-    public:
-        vector<double> weights;
-        vector<double> biases;
 
+    private:
+
+        //IMP Vars
+        double input1[10] = {2,2,2,2,2};
+        double weight1[10] = {2,2,2,2,2};
+        double bias[10] = {2,2,2,2,2};
+
+    public:
+
+        //Creating Vectors for weights and biases
+        vector<double> weights;
+        vector<double> inputs;
+        double biasN;
+
+        //Constructor
+        Node(int i){
+
+            //The Loop enters all the inputs and weights from the array into their respective vectors
+            for (int i = 0; i < end(weight1)-begin(weight1); i++)
+            {
+                weights.at(i-1) = weight1[i-1];
+                inputs.at(i-1) = input1[i-1];
+                
+            }
+            
+            //The bias value of a particular Node
+            biasN = bias[i];
+
+        }
 };
 
 
 class Layer{
     public:
+
+        //Vars
         int numNodesIn,numNodesOut;
         vector<Node> nodes;
         
@@ -25,17 +54,27 @@ class Layer{
             this->numNodesIn = NodesIn;
             this->numNodesOut = NodesOut;
 
+            //Creates and adds Nodes as per requirement
             for (int i = 0; i < numNodesOut; i++)
             {
-                nodes.insert();
+
+                //Creates a object of class Node
+                Node N(i);
+
+                //Adds the object in the vector
+                nodes.push_back(N);
+
+
             }
             
         } 
 };
 
+
+//Main Function
 int main() {
   
-  long double out =  input[0]*weight[0] + input[1]*weight[1] + input[2]*weight[2] + input[3]*weight[3] + input[4]*weight[4] + bias[0];
+//   long double out =  input[0]*weight[0] + input[1]*weight[1] + input[2]*weight[2] + input[3]*weight[3] + input[4]*weight[4] + bias[0];
 
-  cout << out;
+//   cout << out;
 }
