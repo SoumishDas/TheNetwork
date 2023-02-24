6f973f18e4474fcc8276cc669cce614e68b6a1c1
soumish <soumish.das@gmail.com>
2023-02-24 23:06:31 +0530

    Fixed Error In ankush's code and Added Header File

--- src/network.cpp
+++ src/network.cpp
@@ -1,138 +1,109 @@
 //Header Files
-#include <iostream>
-#include <vector>
-#include <cstdlib>
-#include <iomanip>
-#include <ctime>
-#include <array>
+#include "network.h"
+
 
 using namespace std;
 
 
 
 
-//Classes
-class Node{
+
+Node::Node(int numNodeIn){
+
+    //The Loop enters all the inputs and weights from the array into their respective vectors
+    for (int i = 0; i < numNodeIn; i++)
+    {
+        double z = (double)(rand() % 100) /100;;
+        //cout << z<<"##"<<endl;
+        weights.push_back(z);
+        
+        
+    }
     
-    public:
+    //The bias value of a particular Node
+    biasN = (double)(rand() % 1000) /100;
+    //cout << biasN<<"#"<<endl;
+}
 
-        //Creating Vectors for weights and biases
-        vector<double> weights;
-        
-        double biasN;
 
-        //Constructor
-        Node(int numNodeIn){
 
-            //The Loop enters all the inputs and weights from the array into their respective vectors
-            for (int i = 0; i < numNodeIn; i++)
-            {
-                double z = (double)(rand() % 100) /100;;
-                cout << z<<"##"<<endl;
-                weights.push_back(z);
-                
-                
-            }
-            
-            //The bias value of a particular Node
-            biasN = (double)(rand() % 1000) /100;
-            cout << biasN<<"#"<<endl;
+
+Layer::Layer(int NodesIn,int NodesOut){
+    this->numNodesIn = NodesIn;
+    this->numNodesOut = NodesOut;
+
+    //Creates and adds Nodes as per requirement
+    for (int i = 0; i < numNodesOut; i++)
+    {
+
+        //Creates a object of class Node
+        Node N(numNodesIn);
+
+        //Adds the object in the vector
+        nodes.push_back(N);
+
+
+    }
+    
+}
+
+
+vector<double> Layer::calcOutput(double inputs[]){
+    //Dynamic array for storing weighted inputs after computation
+    vector<double> weightedInputs ;
+
+    //For loop to iterate over the Outnodes and compute weighted inputs
+    for (int nodeOut = 0; nodeOut < numNodesOut; nodeOut++)
+    {
+        // Adding Bias to the weighted input
+        double weightedInput = nodes[nodeOut].biasN;
+
+        // Adding all the inputs multiplied with weights 
+        for (int nodeIn = 0; nodeIn < numNodesIn; nodeIn++)
+        {
+            weightedInput += inputs[nodeOut] * nodes[nodeOut].weights[nodeIn];
         }
-};
+        weightedInputs.push_back(weightedInput);
 
 
-class Layer{
-    public:
-
-        //Vars
-        int numNodesIn,numNodesOut;
-        vector<Node> nodes;
-        
-        Layer(int NodesIn,int NodesOut){
-            this->numNodesIn = NodesIn;
-            this->numNodesOut = NodesOut;
-
-            //Creates and adds Nodes as per requirement
-            for (int i = 0; i < numNodesOut; i++)
-            {
-
-                //Creates a object of class Node
-                Node N(numNodesIn);
-
-                //Adds the object in the vector
-                nodes.push_back(N);
-
-
-            }
-            
-        }
-        vector<double> calcOutput(double inputs[]){
-            //Dynamic array for storing weighted inputs after computation
-            vector<double> weightedInputs ;
-
-            //For loop to iterate over the Outnodes and compute weighted inputs
-            for (int nodeOut = 0; nodeOut < numNodesOut; nodeOut++)
-            {
-                // Adding Bias to the weighted input
-                double weightedInput = nodes[nodeOut].biasN;
-
-                // Adding all the inputs multiplied with weights 
-                for (int nodeIn = 0; nodeIn < numNodesIn; nodeIn++)
-                {
-                    weightedInput += inputs[nodeOut] * nodes[nodeOut].weights[nodeIn];
-                }
-                weightedInputs.push_back(weightedInput);
-
-
-            }
-            return weightedInputs;
-        }
-};
-
-class Neural_Net{
-
-    public:
-
-        //Variables
-        vector<Layer> layout;
-        int inputSize;
-        int arraySize;
-        int outputSize;
-
-        Neural_Net(int a[], int S){
-
-            arraySize = S;
-            inputSize = a[0];
-            outputSize = a[S-1];
-
-            for (int i = 0; i < S-1; i++)
-            {
-
-                //Creates a object of class Layer
-                Layer L(a[i],a[i+1]);
-
-                //Adds the object in the vector
-                layout.push_back(L);
-
-
-            }
+    }
+    return weightedInputs;
+}
 
 
 
-            //Testing
-            // cout<<arraySize<<" ";
-            // cout<<outputSize<<" ";
-            // cout<<inputSize;
+Neural_Net::Neural_Net(vector<int> a){
 
-        }
-};
+    int S = a.size();
+    inputSize = a[0];
+    outputSize = a[S-1];
+
+    for (int i = 0; i < S-1; i++)
+    {
+
+        //Creates a object of class Layer
+        Layer L(a[i],a[i+1]);
+
+        //Adds the object in the vector
+        layout.push_back(L);
+
+
+    }
+
+
+
+    //Testing
+    // cout<<arraySize<<" ";
+    // cout<<outputSize<<" ";
+    // cout<<inputSize;
+
+}
+
 
 
 //Main Function
 int main() {
   
-//   long double out =  input[0]*weight[0] + input[1]*weight[1] + input[2]*weight[2] + input[3]*weight[3] + input[4]*weight[4] + bias[0];
-
     // Setting random seed as current time
     // srand (static_cast <unsigned> (time(0)));
 
@@ -140,8 +111,8 @@ int main() {
     // double inp[3] = {2.5,2.5,1.5};
     // vector<double> out = layer.calcOutput(inp);
     
-    int a[3] = {2,3,2};
+    vector<int> a = {2000,30000,3000,3000,200};
 
-    Neural_Net NN(a, sizeof(a)/sizeof(a[0]));
+    Neural_Net NN(a);
     
 }
--- src/network.h
+++ src/network.h
@@ -1,3 +1,6 @@
+#ifndef NETWORK_H
+#define NETWORK_H
+
 //Header Files
 #include <iostream>
 #include <vector>
@@ -22,22 +25,7 @@ class Node{
         double biasN;
 
         //Constructor
-        Node(int numNodeIn){
-
-            //The Loop enters all the inputs and weights from the array into their respective vectors
-            for (int i = 0; i < numNodeIn; i++)
-            {
-                double z = (double)(rand() % 100) /100;;
-                cout << z<<"##"<<endl;
-                weights.push_back(z);
-                
-                
-            }
-            
-            //The bias value of a particular Node
-            biasN = (double)(rand() % 1000) /100;
-            cout << biasN<<"#"<<endl;
-        }
+        Node(int numNodeIn);
 };
 
 
@@ -48,45 +36,8 @@ class Layer{
         int numNodesIn,numNodesOut;
         vector<Node> nodes;
         
-        Layer(int NodesIn,int NodesOut){
-            this->numNodesIn = NodesIn;
-            this->numNodesOut = NodesOut;
-
-            //Creates and adds Nodes as per requirement
-            for (int i = 0; i < numNodesOut; i++)
-            {
-
-                //Creates a object of class Node
-                Node N(numNodesIn);
-
-                //Adds the object in the vector
-                nodes.push_back(N);
-
-
-            }
-            
-        }
-        vector<double> calcOutput(double inputs[]){
-            //Dynamic array for storing weighted inputs after computation
-            vector<double> weightedInputs ;
-
-            //For loop to iterate over the Outnodes and compute weighted inputs
-            for (int nodeOut = 0; nodeOut < numNodesOut; nodeOut++)
-            {
-                // Adding Bias to the weighted input
-                double weightedInput = nodes[nodeOut].biasN;
-
-                // Adding all the inputs multiplied with weights 
-                for (int nodeIn = 0; nodeIn < numNodesIn; nodeIn++)
-                {
-                    weightedInput += inputs[nodeOut] * nodes[nodeOut].weights[nodeIn];
-                }
-                weightedInputs.push_back(weightedInput);
-
-
-            }
-            return weightedInputs;
-        }
+        Layer(int NodesIn,int NodesOut);
+        vector<double> calcOutput(double inputs[]);
 };
 
 class Neural_Net{
@@ -99,30 +50,9 @@ class Neural_Net{
         int arraySize;
         int outputSize;
 
-        Neural_Net(int a[], int S){
-
-            arraySize = S;
-            inputSize = a[0];
-            outputSize = a[S-1];
-
-            for (int i = 0; i < S-1; i++)
-            {
-
-                //Creates a object of class Layer
-                Layer L(a[i],a[i+1]);
-
-                //Adds the object in the vector
-                layout.push_back(L);
-
-
-            }
-
-
-
-            //Testing
-            // cout<<arraySize<<" ";
-            // cout<<outputSize<<" ";
-            // cout<<inputSize;
-
-        }
+        Neural_Net(vector<int> a);
 };
+
+
+
+#endif
